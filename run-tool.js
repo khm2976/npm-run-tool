2252fdc865f66abfee93858960c5f9754aa9a64b
khm2976 <khm2976@gmail.com>
2020-08-01 17:57:38 +0900

    commander default

--- run-tool.js
+++ run-tool.js
@@ -1,4 +1,12 @@
 const program = require('commander');
 const list = require('./list');
 
-list();
+// list(); -> 커맨드 list로 처리
+program
+    .command('list')    // 커맨드 이름
+    .alias('ls')        // 커맨드 축약형
+    .description('show categorylist')  // 설명
+    .action(function () {
+        list();
+    });
+program.parse(process.argv);
